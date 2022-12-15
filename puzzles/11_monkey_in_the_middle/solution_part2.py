61d26239d3381826bf1e30e3d66803da38c3ff53
Dan MacLean <dan.maclean@hardrockdigital.com>
2022-12-15 11:45:02 -0500

    Finally solved it

--- puzzles/11_monkey_in_the_middle/solution_part2.py
+++ puzzles/11_monkey_in_the_middle/solution_part2.py
@@ -1,41 +1,20 @@
 import dataclasses
 import time
-from typing import List, Union
+from typing import List
 
 
 @dataclasses.dataclass
 class Item:
-    number: int
-    multiplier: int
-    remainder: int
-    real_value: int
+    worry_level: int
 
-    def multiply(self, factor) -> None:
-        if isinstance(factor, Item):
-            self.multiplier *= factor.number * factor.multiplier
-        else:
-            self.multiplier *= factor
-            self.remainder *= factor
-            self.real_value *= factor
+    def multiply(self, factor: int) -> None:
+        self.worry_level *= factor
 
     def add(self, factor) -> None:
-        self.remainder += factor
-        if self.remainder >= self.number:
-            self.multiplier += self.remainder // self.number
-            self.remainder = self.remainder % self.number
-        self.real_value += factor
+        self.worry_level += factor
 
     def is_divisible(self, factor) -> bool:
-        if self.real_value != (self.number * self.multiplier + self.remainder):
-            print('values not same')
-        if self.number % factor == 0 and self.remainder == 0:
-            return True
-        elif self.multiplier % factor == 0 and self.remainder == 0:
-            return True
-        elif (self.number * self.multiplier + self.remainder) % factor == 0:
-            return True
-
-        return False
+        return self.worry_level % factor == 0
 
 
 @dataclasses.dataclass
@@ -50,7 +29,7 @@ class Monkey:
     inspections: int = 0
 
     def calc_worry_level(self, item: Item) -> None:
-        op2 = item if self.operand_2 == -999 else self.operand_2
+        op2 = item.worry_level if self.operand_2 == -999 else self.operand_2
         if self.op == '*':
             item.multiply(op2)
         elif self.op == '+':
@@ -66,6 +45,8 @@ class Monkey:
         while self.items:
             item = self.items.pop(0)
             self.calc_worry_level(item)
+            if item.worry_level > all_monkey_divisibility:
+                item.worry_level = item.worry_level % all_monkey_divisibility
 
             if item.is_divisible(self.test):
                 monkeys[self.true_monkey].receive_item(item)
@@ -75,7 +56,7 @@ class Monkey:
 
 
 monkeys = []
-with open('sample_input.txt') as f:
+with open('input.txt') as f:
     starting_items = []
     operand_1 = None
     op = None
@@ -87,7 +68,7 @@ with open('sample_input.txt') as f:
         trimmed = line.strip()
         if trimmed.find('Starting items') > -1:
             items_str = str(trimmed[trimmed.find(':') + 1:]).strip()
-            starting_items = [Item(int(x), 1, 0, int(x)) for x in items_str.split(', ')]
+            starting_items = [Item(int(x)) for x in items_str.split(', ')]
         elif trimmed.find('Operation: ') > -1:
             parts = str(trimmed[trimmed.find('=') + 1:]).strip().split(' ')
             if parts[0] != 'old':
@@ -108,11 +89,14 @@ with open('sample_input.txt') as f:
             monkey = Monkey(starting_items, operand_1, op, operand_2, test, true_monkey, false_monkey)
             monkeys.append(monkey)
 
-for r in range(0, 10_000):
+all_monkey_divisibility = 1
+for m in monkeys:
+    all_monkey_divisibility *= m.test
+
+for r in range(0, 10000):
     start = time.time()
     for monkey in monkeys:
         monkey.evaluate()
-    print(f'Finished round {r + 1} in {time.time() - start} seconds')
 
 monkey_business_factor = 1
 for inspections in sorted([monkey.inspections for monkey in monkeys], reverse=True)[:2]:
