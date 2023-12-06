16e2941e401c85ba59b0031e4b56d41fcdb1d654
Dan MacLean <dan.maclean@hardrockdigital.com>
2023-12-05 21:48:00 -0600

    Part 1

--- puzzles/2023/day05_fertilizer/solution_part1.py
+++ puzzles/2023/day05_fertilizer/solution_part1.py
@@ -1,17 +1,51 @@
+import dataclasses
 import re
-from typing import Dict
+from collections import namedtuple
+from typing import List, Optional
+
+MapRange = namedtuple("MapRange", ["dest", "source", "range"])
+
+
+@dataclasses.dataclass
+class MapRange:
+    dest: int
+    source: int
+    range: int
+
+    def dest_for_source(self, val: int) -> Optional[int]:
+        if self.source <= val <= self.source + self.range:
+            inc = val - self.source
+            return self.dest + inc
+        else:
+            return None
+
+
+class Ranges:
+
+    def __init__(self):
+        super().__init__()
+        self.ranges: List[MapRange] = []
+
+    def add(self, m: MapRange) -> None:
+        self.ranges.append(m)
+
+    def dest_for_source(self, val: int) -> int:
+        for r in self.ranges:
+            if r.dest_for_source(val):
+                return r.dest_for_source(val)
+        return val
 
 
 def main():
-    with open("sample_input.txt") as f:
+    with open("input.txt") as f:
         seeds = []
-        seed_to_soil = {}
-        soil_to_fertilizer = {}
-        fertilizer_to_water = {}
-        water_to_light = {}
-        light_to_temp = {}
-        temp_to_humidity = {}
-        humidity_to_loc = {}
+        seed_to_soil = Ranges()
+        soil_to_fertilizer = Ranges()
+        fertilizer_to_water = Ranges()
+        water_to_light = Ranges()
+        light_to_temp = Ranges()
+        temp_to_humidity = Ranges()
+        humidity_to_loc = Ranges()
 
         d_of_d = {
             "seed-to-soil": seed_to_soil,
@@ -44,20 +78,27 @@ def main():
 
         locations = []
         for seed in seeds:
-            soil = seed_to_soil.get(seed, seed)
-            fertilizer = soil_to_fertilizer.get(soil, soil)
-            water = fertilizer_to_water.get(fertilizer, fertilizer)
-            light = water_to_light.get(water, water)
-            temp = light_to_temp.get(light, light)
-            humidity = temp_to_humidity.get(temp, temp)
-            locations.append(humidity_to_loc.get(humidity, humidity))
+            soil = seed_to_soil.dest_for_source(seed)
+            fertilizer = soil_to_fertilizer.dest_for_source(soil)
+            water = fertilizer_to_water.dest_for_source(fertilizer)
+            light = water_to_light.dest_for_source(water)
+            temp = light_to_temp.dest_for_source(light)
+            humidity = temp_to_humidity.dest_for_source(temp)
+            locations.append(humidity_to_loc.dest_for_source(humidity))
+            # soil = seed_to_soil.get(seed, seed)
+            # fertilizer = soil_to_fertilizer.get(soil, soil)
+            # water = fertilizer_to_water.get(fertilizer, fertilizer)
+            # light = water_to_light.get(water, water)
+            # temp = light_to_temp.get(light, light)
+            # humidity = temp_to_humidity.get(temp, temp)
+            # locations.append(humidity_to_loc.get(humidity, humidity))
         print(min(locations))
 
 
-def populate_dict_from_line(trimmed: str, d: Dict) -> None:
+def populate_dict_from_line(trimmed: str, d: Ranges) -> None:
     nums = [int(n) for n in trimmed.split(" ")]
-    for i in range(0, nums[2]):
-        d[nums[1] + i] = nums[0] + i
+    m = MapRange(nums[0], nums[1], nums[2])
+    d.add(m)
 
 
 if __name__ == '__main__':
